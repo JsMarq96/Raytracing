4724227988970f6a65330525d70f7189eaeba7bc
Juan S. Marquerie <juansebastian.marquerie01@estudiant.upf.edu>
2022-03-07 20:04:01 +0100

    Added a raycast function to the rt_scene

--- src/rt_primitives.h
+++ src/rt_primitives.h
@@ -19,7 +19,7 @@ enum eRT_Prim : uint8_t {
 // ===================
 
 // ? is this okay?
-inline bool ray_sphere_collision(const sVector3 &ray_origin,
+inline float ray_sphere_collision(const sVector3 &ray_origin,
                                  const sVector3 &ray_dir,
                                  const sTransform &sphere_transform) {
     float radius = sphere_transform.scale.x;
@@ -28,9 +28,15 @@ inline bool ray_sphere_collision(const sVector3 &ray_origin,
     float b = 2.0f * dot_prod(ray_dir, L);
     float c = dot_prod(L, L) - (radius * radius);
 
-    float res_1 = 0.0f, res_2 = 0.0f;
+    float res_1 = -1.0f, res_2 = -1.0f;
 
-    return solve_quadratic_equation(a, b, c, &res_1, &res_2);
+    if (solve_quadratic_equation(a, b, c, &res_1, &res_2)) {
+        // We only care (FOR NOW) of the contact point time, and that
+        // is the firsdt result of the equation
+        return res_1;
+    }
+
+    return -1.0f;
 }
 
 // ===================
--- src/rt_scene.h
+++ src/rt_scene.h
@@ -53,37 +53,29 @@ struct sRT_Scene {
 
         for (uint32_t x = 0; x < render_width; x++) {
             for (uint32_t y = 0; y < render_heigth; y++) {
+                // Copmute NDC coords
                 float u = ((float) x / render_width) - 0.5f;
                 float v = ((float) y / render_heigth) - 0.5f;
                 u *= aspect_ratio;
 
+                // Compute the viewpoint of the NDC
                 sVector3 ray_dir = {u, v, -tan_half_FOV};
+                // To world view <- is this ok..???
                 ray_dir = camera.view_mat.multiply(ray_dir);
 
-                // Raytracing
-                // TODO: extract more info from the collision: point, depht...
 
                 // Compute the background gradient
                 float t =0.5f * (ray_dir.normalize().y + 1.0f);
                 uColor_RGBA8 out_color = LERP(sky_color_1, sky_color_2, t);
 
-                for(uint16_t i = 0; i <  RT_OBJ_COUNT; i++) {
-                    if (!is_obj_enabled[i]) {
-                        continue;
-                    }
+                sVector3 col_point = {};
+                uint16_t col_obj_id = 0;
 
-                    bool hit = false;
-                    switch(obj_primitive[i]) {
-                        case RT_SPHERE: hit = ray_sphere_collision(ray_origin,
-                                                                   ray_dir,
-                                                                   obj_transforms[i]); break;
-                        case RT_PLANE: break;
-                        case RT_PRIMITIVE_COUNT: break; // For the warning
-                    }
-
-                    if (hit) {
-                        out_color = obj_color[i];
-                    }
+                if (raycast(ray_origin,
+                            ray_dir,
+                            &col_point,
+                            &col_obj_id)) {
+                    out_color = obj_color[col_obj_id];
                 }
                 //std::cout << out_depth << std::endl;
                 // Store color on texture
@@ -98,6 +90,48 @@ struct sRT_Scene {
                                      GL_UNSIGNED_BYTE);
 
         free(raw_fbuffer);
+    }
+
+    inline bool raycast(const sVector3 &ray_origin,
+                        const sVector3 &ray_direction,
+                        sVector3 *collision_point,
+                        uint16_t *object_id) const {
+        uint16_t it_col_object = 0;
+        float it_t = 1000.0f;
+        for(uint16_t i = 0; i < RT_OBJ_COUNT; i++) {
+            if (!is_obj_enabled[i]) {
+                continue;
+            }
+
+            float t_of_hit = -1.0f;
+            // Test if the ray hits the object
+            switch (obj_primitive[i]) {
+                case RT_SPHERE: t_of_hit = ray_sphere_collision(ray_origin,
+                                                                ray_direction,
+                                                                obj_transforms[i]); break;
+                case RT_PLANE: break;
+                case RT_PRIMITIVE_COUNT: break; // Just for removing the warning
+            }
+
+            if (t_of_hit > 0.0f) {
+                // A hit has happend
+                if (it_t > t_of_hit) {
+                    // The hit is closer, so we keep it
+                    it_t = t_of_hit;
+                    it_col_object = i;
+                }
+            }
+        }
+
+        if (it_t < 1000.0f) {
+            // A hit has happend
+            *object_id = it_col_object;
+            // Calculate the position that the ray would have at the time of the
+            // collision
+            *collision_point = ray_direction.normalize().mult(it_t).sum(ray_origin);
+            return true;
+        }
+        return false;
     }
 };
 
